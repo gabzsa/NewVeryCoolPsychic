f8ca5ebf4a3f2708c40ba053c96f36ef2c52876a
gabzsa <109770528+gabzsa@users.noreply.github.com>
2023-03-21 16:13:07 -0300

    F

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -132,6 +132,10 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		changeSelection();
 		reloadCheckboxes();
+		#if android
+		addVirtualPad(LEFT_FULL, A_B);
+		addPadCamera();
+		#end
 	}
 
 	var nextAccept:Int = 5;
