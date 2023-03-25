444549f2d042cf9c7aa685f150e951c7c0d21856
gabzsa <109770528+gabzsa@users.noreply.github.com>
2023-03-25 10:50:22 -0300

    Fa

--- source/Paths.hx
+++ source/Paths.hx
@@ -233,7 +233,7 @@ class Paths
 	
 	static public function getTextFromFile(key:String, ?ignoreMods:Bool = false):String
 	{
-		#if sys
+		#if desktop
 		if (!ignoreMods && FileSystem.exists(mods(key)))
 			return File.getContent(mods(key));
 		#end
