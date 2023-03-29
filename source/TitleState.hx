d8bc1586a77eb30b455a2a674202dfc99ae1a6f7
gabzsa <109770528+gabzsa@users.noreply.github.com>
2023-03-29 16:04:47 -0300

    Update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -15,7 +15,7 @@ import flixel.addons.transition.TransitionData;
 import haxe.Json;
 import openfl.display.Bitmap;
 import openfl.display.BitmapData;
-#if MODS_ALLOWED
+#if desktop
 import sys.FileSystem;
 import sys.io.File;
 #end
