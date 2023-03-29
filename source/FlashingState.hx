dd58af9c151081cc0fe38456703983e276f62ccd
gabzsa <109770528+gabzsa@users.noreply.github.com>
2023-03-29 16:00:51 -0300

    Que9

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -28,8 +28,7 @@ class FlashingState extends MusicBeatState
 			This Mod contains some flashing lights!\n
 			Press ENTER to disable them now or go to Options Menu.\n
 			Press ESCAPE to ignore this message.\n
-			You've been warned!
-            Mod Ported By Gabseila",
+			You've been warned!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
